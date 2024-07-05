db2f2a0627b0cabfc8c2ea3e63b1525805618f81
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2024-07-05 08:08:37 +0530

    further got introduce typescript

--- src/a.ts
+++ src/a.ts
@@ -62,3 +62,68 @@ function child(name:string) {
 }
 
 parentFunv(child);
+
+
+
+
+
+
+
+function runAfter1s(fn: () => void){
+  setTimeout(fn, 1000)
+}
+
+const doSomething: () => void =() =>{
+  console.log("Hello")
+  return 5
+}
+
+runAfter1s(doSomething)
+//How to give types to an object when there is function which returns true or false
+//if a user is above 18.
+//withoutInterface
+
+
+//Here in below code you can see if you have to give the type of each object value and you have to write it again each time you use it in a function.
+//On the other had you can create an Interface and then just re-use it where you need it. first letter must be CAPITAL whie creating an Interface.
+
+
+
+function ifLegal(user: {
+firstName: string;
+  lastName: string;
+  age: number;
+}){
+  console.log("Hi there " + user.firstName);
+}
+
+//Interface
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number
+}
+
+function isLegal (user: User){
+  if(user.age > 18){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function greetUser(user: User):void {
+  console.log("Hi there " + user.firstName)
+}
+
+const user: User = ({
+  firstName: "Ashish",
+  lastName: "Gupta",
+  age: 24
+
+})
+console.log(isLegal(user));
+greetUser(user);
+
+
+
